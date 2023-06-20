94964852feae20003c6464b077fabc4b80cc3d6f
kiranthilak <kiran.thilak@yahoo.com>
2023-06-20 15:58:59 +0530

    clean up

---
+++ src/CuratedService.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CuratedService = () => {
+  return (
+    <div className="bg-[#69C5FF]">
+      <h1 className="text-3xl font-bold text-white break-words">
+        Curated Services
+      </h1>
+      <h3 className="text-2xl font-bold text-white break-words">
+        Pan India Network
+      </h3>
+    </div>
+  );
+};
+
+export default CuratedService;

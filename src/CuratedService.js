011828e2628dd5e0719317c2569f166d50f0483b
kiranthilak <kiran.thilak@yahoo.com>
2023-06-21 09:22:39 +0530

    size changes

--- src/CuratedService.js
+++ src/CuratedService.js
@@ -1,5 +1,7 @@
 import React from "react";
-import curatedservice1 from "./img/curatedservice1.png";
+/* import curatedservice1 from "./img/curatedservice1.png"; */
+import curatedservice2x from "./img/curatedservice2x.png";
+/* import curatedservice4x from "./img/curatedservice4x.png"; */
 /* import curatedservice2 from "./img/curatedservice2.png"; */
 import Curatedcards from "./Curatedcards";
 import iconImage from "./img/batterycharging.png";
@@ -36,27 +38,27 @@ const CuratedService = () => {
   return (
     <div className="bg-[#69C5FF]">
       <div className="flex items-center">
-        <div className="w-full lg:w-2/3 relative pl-10 ">
-          <h1 className="text-3xl font-bold text-white whitespace-normal ">
+        <div className="w-full  relative pl-10 ">
+          <h1 className="text-6xl font-bold text-white whitespace-normal ">
             Curated <br /> Services
           </h1>
           <button className="py-2 px-6 mt-4 bg-yellow-500 text-white rounded-lg ">
             View More
           </button>
-          <h3 className="text-2xl font-bold text-white whitespace-normal py-2 ">
+          <h3 className="text-5xl font-bold text-white whitespace-normal py-2 ">
             Pan India <br /> Network
           </h3>
         </div>
 
-        <div className="w-full  lg:w-1/3 relative">
+        <div className="w-full   relative">
           {/*  <img
             className="h-auto max-w-full absolute bottom-2 left-2 z-10"
             src={curatedservice2}
             alt="First"
           /> */}
           <img
-            className="h-auto max-w-full relative z-0"
-            src={curatedservice1}
+            className="h-auto  max-w-full w-full relative z-0"
+            src={curatedservice2x}
             alt="Second"
           />
         </div>
